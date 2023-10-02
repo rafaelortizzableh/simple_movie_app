05359d0c7b7d4c2bf60e21ae6b7b895238f30da4
Rafael Ortiz Zableh <57945332+rafaelortizzableh@users.noreply.github.com>
2023-10-03 00:52:20 +0200

    chore: add android specific style and permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.white,
+    ),
+  );
   runApp(const ProviderScope(child: SimpleMovieApp()));
 }
